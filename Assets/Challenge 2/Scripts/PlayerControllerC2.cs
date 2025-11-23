30a96df956a60874a164c78b64a652631b6caa19
Mr-Triangle <dox.cactus@yandex.ru>
2025-11-24 00:47:37 +0500

    mission Basic Gameplay

---
+++ Assets/Challenge 2/Scripts/PlayerControllerC2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerC2 : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public float dogSpawnColldown = 1f;
+    private float timer = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > dogSpawnColldown)
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, transform.rotation);
+                timer = 0;
+            }
+        }
+    }
+}
---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    [SerializeField] private Vector3 position = new Vector3(3, 4, 1);
+    [SerializeField] private float rotateSpeed = 10f;
+    [SerializeField] private float colorRepeating = 1f;
+    private Material material;
+    //[SerializeField] private Vector3 localPosition;
+
+    void Start()
+    {
+        transform.position = position;
+        //transform.localScale = Vector3.one * 1.3f;
+        
+        material = Renderer.material;
+
+        InvokeRepeating("ChangeColor", 0, colorRepeating);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(rotateSpeed * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    void ChangeColor()
+    {
+        material.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+}
